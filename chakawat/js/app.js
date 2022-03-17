1cfa9e2917ec0270556e0c8385a8c0e3006cdc00
Chakawat Chansuwan(Pepo) <101080784+Chakawat@users.noreply.github.com>
2022-03-17 18:58:10 +0700

    NO00 check code (20220317)

---
+++ chakawat/js/app.js
@@ -0,0 +1,17 @@
+$(document).on("ready", function () {
+  $(".spring__dayly__slider__slider").slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+  });
+});
+
+$(document).on("ready", function () {
+  $(".graph__belle__slider__slider").slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+  });
+});
